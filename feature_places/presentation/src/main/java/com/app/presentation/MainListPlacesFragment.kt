ceef011e78ed40563727e4dc93dfbbdf0ea3ab15
albert montes anccasi <albertmontes>
2022-05-15 03:40:55 -0500

    config modules,gradle, nac_graph

---
+++ feature_places/presentation/src/main/java/com/app/presentation/MainListPlacesFragment.kt
@@ -0,0 +1,12 @@
+package com.app.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class ListPlacesFragment : Fragment(R.layout.fragment_main_list_places) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+}
---
+++ lib/src/main/java/com/app/lib/MyClass.kt
@@ -0,0 +1,4 @@
+package com.app.lib
+
+class MyClass {
+}
